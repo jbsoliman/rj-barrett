b0638c5d2f4dc375ae5f44416dc29442fe291fc7
jbsoliman <jsolimaan@gmail.com>
2019-11-04 15:50:58 -0800

    updated indexes to correctly grab rookie data

--- scraper.py
+++ scraper.py
@@ -12,16 +12,16 @@ html = urlopen(url)
 soup = BeautifulSoup(html)
 
 
-# use findALL() to get the column headers
-soup.findAll('tr', limit=2)
+# use findALL() to get the column headers that we need
+soup.findAll('tr',limit=2)[2:]
 # use getText()to extract the text we need into a list
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[1].findAll('th')]
+# exclude the second column as we will not need the ranking order from Basketball Reference for the analysis
 headers = headers[1:]
 headers
 
-# avoid the first header row
-rows = soup.findAll('tr')[1:]
+# avoid the first two header rows
+rows = soup.findAll('tr')[3:]
 player_stats = [[td.getText() for td in rows[i].findAll('td')]
             for i in range(len(rows))]
 
