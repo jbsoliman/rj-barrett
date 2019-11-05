063f4e76d2a6fd71c9f99812a25e8c0077d71ab1
jbsoliman <jsolimaan@gmail.com>
2019-11-04 16:10:24 -0800

    hardcoding bballref pages for sorting

--- scraper.py
+++ scraper.py
@@ -3,16 +3,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-# NBA season we will be analyzing
-year = 2020
-# URL page we will scraping (see image above)
-url = "https://www.basketball-reference.com/leagues/NBA_{}_rookies.html".format(year)
+# URL page we will scraping (rookie sorted by PPG)
+url = "https://www.basketball-reference.com/pi/shareit/wvK58"
 # this is the HTML from the given URL
 html = urlopen(url)
 soup = BeautifulSoup(html)
 
 
-# use findALL() to get the column headers that we need
+# use findALL() to get the column headers
 soup.findAll('tr',limit=2)[2:]
 # use getText()to extract the text we need into a list
 headers = [th.getText() for th in soup.findAll('tr', limit=2)[1].findAll('th')]
@@ -21,12 +19,11 @@ headers = headers[1:]
 headers
 
 # avoid the first two header rows
-rows = soup.findAll('tr')[3:]
+rows = soup.findAll('tr')[2:]
 player_stats = [[td.getText() for td in rows[i].findAll('td')]
             for i in range(len(rows))]
 
 stats = pd.DataFrame(player_stats, columns = headers)
-stats.head(10)
 
 print (stats)
 
